a15ba54089a39380f8c9ce04e309cbd507caba5e
emeraldtip <emeraldtipyt@gmail.com>
2024-04-07 14:12:06 +0300

    voterID generation implemented

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,25 +1,23 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import net.earthmc.emcapiclient.EMCAPIClient;
 import net.earthmc.emcapiclient.object.identifier.DiscordIdentifier;
-import net.earthmc.emcapiclient.object.identifier.PlayerIdentifier;
 import org.simpleyaml.configuration.Configuration;
 import org.simpleyaml.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.EnumSet;
-import java.util.List;
 
 public class main extends ListenerAdapter {
 
@@ -28,10 +26,9 @@ public class main extends ListenerAdapter {
         new main().init();
     }
 
+    public static Configuration configuration;
     public void init() {
-        //token is inside the config.yml file
-        Configuration configuration;
-
+        //config loading
         try {
             configuration = YamlConfiguration.loadConfiguration(new File("config.yml"));
         } catch (IOException e) {
@@ -39,6 +36,7 @@ public class main extends ListenerAdapter {
             return;
         }
 
+        //JDA setup stuff
         EnumSet<GatewayIntent> intents = EnumSet.of(
                 GatewayIntent.GUILD_MESSAGES,
                 GatewayIntent.GUILD_MESSAGE_REACTIONS,
@@ -65,8 +63,7 @@ public class main extends ListenerAdapter {
     }
 
     @Override
-    public void onSlashCommandInteraction(SlashCommandInteractionEvent event)
-    {
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         switch (event.getName()) {
             case "ping" -> pingCommand(event);
             case "voterid" -> idCommand(event);
@@ -74,30 +71,74 @@ public class main extends ListenerAdapter {
         }
     }
 
+    //different utils ----------------------------------------------------------------------
+    private String generateID(String discID, String mcID) {
+        String seed = configuration.getString("seed");
+        String input  = seed+discID+mcID; //combine all of em together
+        try {
+            //hashing the data
+            byte[] hashedData = MessageDigest.getInstance("SHA-256")
+                    .digest(input.getBytes(StandardCharsets.UTF_8));
 
-    //commands all from here on out --------------------------------------------------------
-    private void pingCommand(SlashCommandInteractionEvent event)
-    {
-        event.reply("pong").queue();
+            //turning the hashed byte array into a string and returning it
+            String output = "";
+            for (int i = 0; i<hashedData.length; i++) {
+                output += Integer.toString( (hashedData[i] & 0xff) + 0x100, 16).substring(1);
+            }
+            return output;
+        }
+        catch (NoSuchAlgorithmException e) { //this should never happen
+            e.printStackTrace();
+        }
+        return "";
     }
 
-    private void idCommand(SlashCommandInteractionEvent event)
-    {
+    private boolean sendDM(User user, String content) {
+        try {
+            user.openPrivateChannel()
+                    .flatMap(channel -> channel.sendMessage(content))
+                    .queue();
+            return true;
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+
+    //commands all from here on out --------------------------------------------------------
+    private void pingCommand(SlashCommandInteractionEvent event) {
+        event.reply("pong").setEphemeral(true).queue();
+    }
+
+    private void idCommand(SlashCommandInteractionEvent event) {
         String discID = event.getUser().getId();
         EMCAPIClient client = new EMCAPIClient();
 
         DiscordIdentifier discData = client.getDiscordIdentifierByString(discID);
         String playerID = discData.getUUID();
-        if (playerID==null)
-        {
+        if (playerID==null) {
+            /*
             event.reply("Your minecraft account is not currently linked to your discord.\n" +
                     "To link your discord account log onto **Aurora** and run the **'/discord link'** command " +
                     "and follow the instructions provided in chat."
             ).queue();
+             */
+            playerID = "61fa89cf-0114-4d1d-9079-fdafecabeaf4";
         }
-        else
-        {
-            event.reply("Your UUID is: "+playerID).queue();
-        }
+        //else {
+            String id = generateID(discID,playerID);
+
+            boolean dmSuccess = sendDM(event.getUser(),"Here's your voterID: "+id+"\nDo not share it with others!");
+            if (dmSuccess) {
+                event.reply("VoterID generation successful! Check your DMs").queue();
+            }
+            else {
+                event.reply("Can't send voterID, please make sure you have the " +
+                        "'Allow direct messages from server members' setting enabled").queue();
+            }
+
+        //}
     }
 }
